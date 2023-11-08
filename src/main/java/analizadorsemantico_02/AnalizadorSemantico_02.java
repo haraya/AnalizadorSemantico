bfc49212d40f0a364fa97604883dfecab4c469d8
Heral <hernan.araya96@outlook.com>
2023-11-08 15:06:13 -0600

    Creando un Analizador Semantico en Java

---
+++ src/main/java/analizadorsemantico_02/AnalizadorSemantico_02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package analizadorsemantico_02;
+
+/**
+ *
+ * @author Hernan Araya
+ */
+public class AnalizadorSemantico_02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
